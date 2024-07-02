ff70faf4ceb880ad007af61ca670b7d8d6422ab3
marina ribeiro <marinalucas0@outlook.com>
2024-07-02 19:04:41 -0300

    Imprimindo uma lista com os cadidatos selecionados

--- Controle-Candidatos/src/candidatura/ProcessoSeletivo.java
+++ Controle-Candidatos/src/candidatura/ProcessoSeletivo.java
@@ -4,7 +4,29 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados() {
+        String candidatos[] = { "Marcele ", "Maycon ", "Talita ", "Paulo ", "Talisson " };
+
+        System.out.println("Imprimindo lista de candidato informando o indice do elemento");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("Meu candidato de n° " + indice + " é " + candidatos[indice]);
+
+            /*
+             * PARA NÍVEL DE USUÁRIO É INTERESSANTE UTILIZAR O INDICE ENTRE (INDICE+1)
+             * ASSIM AO EXECUTAR A APLICAÇÃO PODEMOS VISUALIZAR OS CADIDATOS A PARTIR DO
+             * NÚMERO 1
+             */
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+        // forma abre abrevida aonde não precisamos demonstrar o indice
+        for (String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
 
     }
 
